c498d534d58acd5352cfa8b3ee1955e943b3bb30
Ivan Timothy Guansing <70355683+GuansingIT@users.noreply.github.com>
2022-08-26 21:56:25 +0800

    Update Weapon.java

--- exercise_1/Weapon.java
+++ exercise_1/Weapon.java
@@ -1,1 +1,14 @@
-
+public class Weapon {
+   // Weapon attributes
+  
+       String name;
+       double damage;
+       int weight;
+       String rarity;
+       String description;
+  
+  // Weapon method
+    public void weaponDescription () {
+      System.out.println("Greetings warrior! This " + name + " can help you to become one of the greatest by contributing its " + damage + " damage!");
+    }
+}
