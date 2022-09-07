d4f352bd72bab25d8b1890ed14ecfabedb63f667
Ivan Timothy Guansing <70355683+GuansingIT@users.noreply.github.com>
2022-09-08 00:17:00 +0800

    Update App.java

--- exercise_3/App.java
+++ exercise_3/App.java
@@ -1,1 +1,31 @@
+// Array implementation of Stack
+public class App {
+  public static void main(String[] args) {
 
+    // Create new Stack Object
+    Stack tshirt_stack = new Stack(5);
+
+    // Insert new elements into the stack
+    element_stack.push("Water element");
+    element_stack.push("Fire element");
+    element_stack.push("Air element");
+    element_stack.push("Nature element");
+
+    // Show the size of the stack
+    System.out.println("The size of the stack is: " + tshirt_stack.size());
+
+    // Show top element in the stack
+    System.out.println("The top of the stack is: " + tshirt_stack.peek());
+
+    // Remove top element in the stack
+    tshirt_stack.pop();
+
+    System.out.println("After pop");
+
+    // Show all elements in Stack
+    tshirt_stack.printStack();
+
+    // Show top element in the stack
+    System.out.println("The top of the stack is: " + element_stack.peek());
+  }
+}
