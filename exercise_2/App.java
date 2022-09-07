51dbff468e64272a7f54b0095ff74264266e3f6b
Ivan Timothy Guansing <70355683+GuansingIT@users.noreply.github.com>
2022-09-08 00:02:06 +0800

    Update App.java

--- exercise_2/App.java
+++ exercise_2/App.java
@@ -6,6 +6,7 @@ class Main {
         LinkedList<String> myPlaylist = new LinkedList<>();
         
         ///// My Music PlayLIST //////
+        
         myPlaylist.add("Young and Inlove");                              // 1st song
         myPlaylist.add("13");                                            // 2nd song
         myPlaylist.add("I still talk to Jesus");                         // 3rd song
@@ -15,7 +16,7 @@ class Main {
         myPlaylist.addLast("Dito, Sa ilalim ng buwan");                  // Additional song at the end of the Playlist
 
         System.out.println(myPlaylist.get(0));                           // Displaying the song at the front using get() and printl() method 
-        myPlaylist.set(6, "Magasin");                                    // Replacing the last song using set() method
+        myPlaylist.set(6, "Stuck with You");                             // Replacing the last song using set() method
         myPlaylist.remove(1);                                            // Removing the first song using remove() method
         System.out.println("Updated playlist: " + myPlaylist);           // Displaying all songs in Playlist using the println method 
     }
