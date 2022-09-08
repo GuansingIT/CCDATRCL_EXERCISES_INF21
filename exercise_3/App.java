cbb8f560576274265255418e3616ba1fc0f873b1
Ivan Timothy Guansing <70355683+GuansingIT@users.noreply.github.com>
2022-09-08 16:22:07 +0800

    Update App.java

--- exercise_3/App.java
+++ exercise_3/App.java
@@ -3,7 +3,7 @@ public class App {
   public static void main(String[] args) {
 
     // Create new Stack Object
-    Stack tshirt_stack = new Stack(5);
+    Stack element_stack = new Stack(5);
 
     // Insert new elements into the stack
     element_stack.push("Blue element");
@@ -12,18 +12,18 @@ public class App {
     element_stack.push("Green element");
 
     // Show the size of the stack
-    System.out.println("The size of the stack is: " + tshirt_stack.size());
+    System.out.println("The size of the stack is: " + element_stack.size());
 
     // Show top element in the stack
-    System.out.println("The top of the stack is: " + tshirt_stack.peek());
+    System.out.println("The top of the stack is: " + element_stack.peek());
 
     // Remove top element in the stack
-    tshirt_stack.pop();
+    element_stack.pop();
 
     System.out.println("After pop");
 
     // Show all elements in Stack
-    tshirt_stack.printStack();
+    element_stack.printStack();
 
     // Show top element in the stack
     System.out.println("The top of the stack is: " + element_stack.peek());
