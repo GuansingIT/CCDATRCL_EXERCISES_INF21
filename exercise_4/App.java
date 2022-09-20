e969fe148c43355ea67bcd61e23ed0c8268e454e
Ivan Timothy Guansing <70355683+GuansingIT@users.noreply.github.com>
2022-09-20 14:36:40 +0800

    Create App.java

---
+++ exercise_4/App.java
@@ -0,0 +1,22 @@
+  Use framework implementation of Java
+	
+Enqueue 5 of your classmates in this section
+	
+Get the size of the queue
+	
+Get the front of the queue
+	
+Dequeue 5 of your classmates 
+	
+Enqueue another 5 of you classmates
+	
+Get the size of the queue
+	
+Get the front of the queue
+	
+Create a exercise_4 folder in your CCDATRCL_EXERCISES_INF214 repository
+	
+Upload the App.java file in the exercise_4 folder
+
+
+
