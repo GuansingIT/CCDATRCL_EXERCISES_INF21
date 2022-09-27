259d0952a580e3a6031f9c9814d0b027718841a4
Ivan Timothy Guansing <70355683+GuansingIT@users.noreply.github.com>
2022-09-27 15:29:11 +0800

    Update Nodes.java

--- exercise_5/Nodes.java
+++ exercise_5/Nodes.java
@@ -1,1 +1,14 @@
+public class Node {
+  
 
+    String office;
+    Node left;
+    Node right;
+
+     Node(String office) {
+
+        this.office = office;
+        left = null;
+        right = null;
+      }
+}
